380dc394d411903001440801834d57a6a2715b5b
Gabriel Bazán <gabrielbazan7@hotmail.com>
2020-01-08 09:52:56 -0300

    fix send max and scanner

--- src/pages/send/send.ts
+++ src/pages/send/send.ts
@@ -25,6 +25,7 @@ import { PayproProvider } from '../../providers/paypro/paypro';
 import { ProfileProvider } from '../../providers/profile/profile';
 
 // Pages
+import { ScanPage } from '../scan/scan';
 import { MultiSendPage } from './multi-send/multi-send';
 
 @Component({
@@ -37,7 +38,6 @@ export class SendPage {
   public wallets = {} as CoinsMap<any>;
   public hasWallets = {} as CoinsMap<boolean>;
   public invalidAddress: boolean;
-  private scannerOpened: boolean;
   private validDataTypeMap: string[] = [
     'BitcoinAddress',
     'BitcoinCashAddress',
@@ -100,15 +100,10 @@ export class SendPage {
   }
 
   public openScanner(): void {
-    this.scannerOpened = true;
-    /* this.walletTabsProvider.setSendParams({
-      amount: this.navParams.data.amount,
-      coin: this.navParams.data.coin
-    });
-    this.walletTabsProvider.setFromPage({ fromSend: true });
-    
-    TODO*/
-    this.events.publish('ScanFromWallet');
+    this.navCtrl.push(ScanPage, { fromSend: true });
+    /*     this.events.publish('ScanFromWallet');
+    TODO 
+    */
   }
 
   public isMultiSend(coin: Coin) {
@@ -128,7 +123,7 @@ export class SendPage {
       );
       isValid =
         this.currencyProvider.getChain(this.wallet.coin).toLowerCase() ==
-          addrData.coin && addrData.network == this.wallet.network;
+        addrData.coin && addrData.network == this.wallet.network;
     }
 
     if (isValid) {
@@ -152,7 +147,7 @@ export class SendPage {
   private redir() {
     this.incomingDataProvider.redir(this.search, {
       amount: this.navParams.data.amount,
-      coin: this.navParams.data.coin
+      coin: this.navParams.data.coin // TODO ???? what is this for ?
     });
     this.search = '';
   }
@@ -262,12 +257,6 @@ export class SendPage {
     this.navCtrl.push(MultiSendPage, {
       wallet: this.wallet
     });
-  }
-
-  public closeCam(): void {
-    if (this.scannerOpened) this.events.publish('ExitScan');
-    // else this.getParentTabs().dismiss();
-    this.scannerOpened = false;
   }
 
   public close(): void {
