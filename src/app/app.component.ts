107435df9052eefe7ab7712175eb41622d645cf0
Juan Pablo Abuin <juanpabloabuin@gmail.com>
2020-02-28 16:48:02 -0300

    Dark mode option. Home tab. First approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -76,6 +76,7 @@ export class CopayApp {
   nav: NavController;
   cardIAB_Ref: InAppBrowser;
   NETWORK = 'livenet';
+  public selectedTheme: string;
   public rootPage:
     | typeof AmountPage
     | typeof DisclaimerPage
@@ -166,6 +167,9 @@ export class CopayApp {
       .load()
       .then(() => {
         this.onAppLoad(readySource);
+        this.appProvider
+          .getActiveTheme()
+          .subscribe(val => (this.selectedTheme = val));
       })
       .catch(err => {
         const title = 'Could not initialize the app';
